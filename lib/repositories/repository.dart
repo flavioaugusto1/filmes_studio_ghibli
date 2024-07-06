56ba31fe3045f872f7eb4fa3b5c77988c15fd334
Flávio <f.augustosdn@gmail.com>
2024-07-06 11:51:31 -0300

    refactor: adicionado tratamento de exceção para repository

--- lib/repositories/repository.dart
+++ lib/repositories/repository.dart
@@ -4,12 +4,17 @@ import 'package:filmes_studio_ghibli/models/filme.dart';
 import 'package:http/http.dart' as http;
 
 Future<List<Filme>> getFilme() async {
-  final response = await http.get(Uri.parse(
-      'https://raw.githubusercontent.com/alura-cursos/obtendo_dados_com_flutter_http/main/filmes.json'));
+  try {
+    final response = await http.get(Uri.parse(
+        'https://raw.githubusercontent.com/alura-cursos/obtendo_dados_com_flutter_http/main/filmes.json'));
 
-  final json = jsonDecode(response.body);
+    final json = jsonDecode(response.body);
 
-  return List<Filme>.from(json.map((elemento) {
-    return Filme.fromJson(elemento);
-  }));
+    return List<Filme>.from(json.map((elemento) {
+      return Filme.fromJson(elemento);
+    }));
+  } catch (e) {
+    print(e);
+    return Future.error("Ocorreu um erro");
+  }
 }
